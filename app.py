e6d9f4e2f5d3de6cf10a003b3f10d905ee489ccc
Olivia Wan <wan.o@husky.neu.edu>
2019-02-09 18:26:47 -0500

    upload file to generate graph

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
+import os
+
 from bokeh.embed import components
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
 
 from hbonds import count_hbonds
-
-import os
-from flask import Flask, flash, request, redirect, url_for, render_template
-from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = '/Users/olivia/Documents/moe'
 ALLOWED_EXTENSIONS = {'csv'}
@@ -18,15 +18,28 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+def get_file():
+    os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
+
+    file = request.files['file']
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return app.config["UPLOAD_FOLDER"], filename
+
+
 @app.route("/", methods=["GET", "POST"])
 def chart():
     fields = count_hbonds.MOE_HEADERS
 
     if request.method == "POST":
-        print(request.files)
+        folder, file = get_file()
+        output_file = count_hbonds.build_output(folder, file)
+        plot = count_hbonds.build_graph(output_file)
+        script, div = components(plot)
+    else:
+        div = ""
+        script = ""
 
-    plot = count_hbonds.build_graph()
-    script, div = components(plot)
-
-    return render_template("index.html",
-                           div=div, script=script, fields=fields)
+    return render_template("index.html", div=div, script=script, fields=fields)
--- hbonds/count_hbonds.py
+++ hbonds/count_hbonds.py
@@ -1,14 +1,25 @@
+import csv
+
 import pandas
 from bokeh.plotting import figure
+import os
 
-data = pandas.read_csv("/Users/olivia/Documents/GitHub/hbonds-web/moe/output.csv")
+moe_dir = "/Users/olivia/Documents/GitHub/hbonds-web/moe"
 
 MOE_HEADERS = ["PDB", "Type", "cb.cb", "sc_.exp_avg", "hb_energy", "Residue.1",
                "Residue.2", "chainId", "expressionHost", "source", "refinementResolution",
                "averageBFactor", "chainLength", "ligandId", "hetId", "residueCount"]
 
 
-def build_graph():
+def build_graph(filters_file=None):
+
+    if filters_file is None:
+        data_file = os.path.join(moe_dir, "output.csv")
+    else:
+        data_file = filters_file
+
+    data = pandas.read_csv(data_file)
+
     # filter out peptides and high-res species
     filtered_data = data[data["hbonds"] > 50]
     filtered_data = filtered_data[data["resolution"] > 1]
@@ -20,3 +31,69 @@ def build_graph():
     p.scatter(x, y)
 
     return p
+
+
+def build_output(upload_folder, filters_file, filter_out):
+    pdbs = []
+    current_hbond = []
+
+    filters = open(os.path.join(upload_folder, filters_file), encoding='utf-8-sig')
+    with filters:
+        reader = csv.DictReader(filters)
+        for i, row in enumerate(reader):
+            pdbs.append(row["PDB"])
+
+    new_row = {
+        "PDB": pdbs[0],
+        "hbonds": 0
+    }
+
+    moe = open(os.path.join(moe_dir, "moe6.csv"))
+    output = open(os.path.join(upload_folder, "output_" + filters_file), "w+")
+
+    with moe, output:
+        reader = csv.DictReader(moe)
+
+        output_headers = ["PDB", "hbonds", "residues", "hbonds/residues", "resolution"]
+        writer = csv.DictWriter(output, fieldnames=output_headers)
+        writer.writeheader()
+
+        for i, row in enumerate(reader):
+            if i % 500000 == 0:
+                print("Reached row %s" % i)
+
+            if filter_out:
+                if row["PDB"] in pdbs:
+                    continue
+            else:
+                if row["PDB"] not in pdbs:
+                    continue
+
+            if row["refinementResolution"] == "NA" or \
+                    float(row["refinementResolution"]) > 4 or \
+                    float(row["chainLength"]) == 0:
+                continue
+
+            if row["PDB"] == new_row["PDB"]:
+                if [row["Residue.1"], row["Residue.2"]] == current_hbond:
+                    continue
+                else:
+                    current_hbond = [row["Residue.1"], row["Residue.2"]]
+                    new_row["hbonds"] = new_row["hbonds"] + 1
+                    if "residues" not in new_row:
+                        new_row["residues"] = row["residueCount"]
+                    if "resolution" not in new_row:
+                        new_row["resolution"] = row["refinementResolution"]
+            else:
+                if "residues" in new_row and "resolution" in new_row:
+                    hbonds = float(new_row["hbonds"])
+                    residues = float(new_row["residues"])
+                    new_row["hbonds/residues"] = hbonds / residues
+                    writer.writerow(new_row)
+
+                new_row = {
+                    "PDB": row["PDB"],
+                    "hbonds": 1
+                }
+
+    return os.path.join(upload_folder, "output_" + filters_file)
