d2073e924f51aa90a712cd2cda19e351c28a4335
Olivia Wan <wan.o@husky.neu.edu>
2019-02-10 16:32:44 -0500

    build means graph

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ def allowed_file(filename):
 
 def get_file():
     os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
-
     file = request.files['file']
 
     if file and allowed_file(file.filename):
@@ -31,19 +30,25 @@ def get_file():
 
 @app.route("/", methods=["GET", "POST"])
 def chart():
-    fields = count_hbonds.MOE_HEADERS
+    scatter_div = ""
+    scatter_script = ""
 
-    div = ""
-    script = ""
+    means_div = ""
+    means_script = ""
 
     if request.method == "POST":
-        if request.form.get("filter-out") is None:
-            filter_out = False
-        else:
-            filter_out = True
+        filter_out = request.form.get("filter-out") is None
         folder, file = get_file()
-        output_file = count_hbonds.build_output(folder, file, filter_out)
-        plot = count_hbonds.build_graph(output_file)
-        script, div = components(plot)
 
-    return render_template("index.html", div=div, script=script, fields=fields)
+        output_file = count_hbonds.build_output(folder, file, filter_out)
+        scatter_plot = count_hbonds.build_full_scatter(output_file)
+        scatter_script, scatter_div = components(scatter_plot)
+
+        means_file = count_hbonds.build_means_output(folder, output_file)
+        # print(means_file)
+        means_plot = count_hbonds.build_means_scatter(means_file)
+        means_script, means_div = components(means_plot)
+
+    return render_template("index.html",
+                           scatter_div=scatter_div, scatter_script=scatter_script,
+                           means_div=means_div, means_script=means_script)
--- hbonds/count_hbonds.py
+++ hbonds/count_hbonds.py
@@ -1,7 +1,9 @@
 import csv
 import os
 
+import numpy as np
 import pandas
+from bokeh.models import Whisker, ColumnDataSource
 from bokeh.plotting import figure
 
 moe_dir = "/Users/olivia/Documents/GitHub/hbonds-web/moe"
@@ -11,7 +13,7 @@ MOE_HEADERS = ["PDB", "Type", "cb.cb", "sc_.exp_avg", "hb_energy", "Residue.1",
                "averageBFactor", "chainLength", "ligandId", "hetId", "residueCount"]
 
 
-def build_graph(filters_file=None):
+def build_full_scatter(filters_file=None):
     if filters_file is None:
         data_file = os.path.join(moe_dir, "output.csv")
     else:
@@ -20,11 +22,11 @@ def build_graph(filters_file=None):
     data = pandas.read_csv(data_file)
 
     # filter out peptides and high-res species
-    filtered_data = data[data["hbonds"] > 50]
-    filtered_data = filtered_data[data["resolution"] > 1]
+    data = data[data["residues"] > 50]
+    data = data[data["resolution"] > 1]
 
-    x = filtered_data["hbonds/residues"]
-    y = filtered_data["resolution"]
+    x = data["hbonds/residues"]
+    y = data["resolution"]
 
     p = figure()
     p.scatter(x, y)
@@ -42,7 +44,8 @@ def build_output(upload_folder, filters_file, filter_out):
             pdbs.append(row["PDB"])
 
     moe = open(os.path.join(moe_dir, "output.csv"))
-    output = open(os.path.join(upload_folder, "output_" + filters_file), "w+")
+    output_filepath = os.path.join(upload_folder, "output_" + filters_file)
+    output = open(output_filepath, "w+")
 
     with moe, output:
         reader = csv.DictReader(moe)
@@ -51,10 +54,7 @@ def build_output(upload_folder, filters_file, filter_out):
         writer = csv.DictWriter(output, fieldnames=output_headers)
         writer.writeheader()
 
-        for i, row in enumerate(reader):
-            if i % 500000 == 0:
-                print("Reached row %s" % i)
-
+        for row in reader:
             if filter_out:
                 if row["PDB"] in pdbs:
                     continue
@@ -64,4 +64,72 @@ def build_output(upload_folder, filters_file, filter_out):
 
             writer.writerow(row)
 
-    return os.path.join(upload_folder, "output_" + filters_file)
+    return output_filepath
+
+
+def build_means_output(upload_folder, output_file):
+    output_file_name = output_file.rsplit("/")[0]
+    means_filepath = os.path.join(upload_folder, "means_" + output_file_name)
+    means_file = open(means_filepath, "w+")
+    csvfile = open(output_file, encoding="utf-8-sig")
+
+    with csvfile, means_file as out:
+        reader = csv.DictReader(csvfile, delimiter=",")
+
+        fieldnames = ["type", "value", "count", "mean", "std"]
+        writer = csv.DictWriter(out, fieldnames=fieldnames)
+        writer.writeheader()
+
+        buckets = {}
+
+        for i, row in enumerate(reader):
+            if float(row["residues"]) <= 50 or float(row["resolution"]) <= 1:
+                continue
+
+            headers = {
+                "hbond_residue": round(float(row["hbonds/residues"]), 3),
+            }
+
+            resolution = float(row["resolution"])
+
+            for key, value in headers.items():
+                pair = (key, float(value))
+                if pair in buckets:
+                    resolutions = buckets[pair]
+                    resolutions.append(resolution)
+                    buckets[pair] = resolutions
+                else:
+                    buckets[pair] = [resolution]
+
+        for key in sorted(buckets.keys()):
+            writer.writerow({
+                "type": key[0],
+                "value": key[1],
+                "count": len(buckets[key]),
+                "mean": np.average(buckets[key]),
+                "std": np.std(buckets[key])
+            })
+
+    return means_filepath
+
+
+def build_means_scatter(data_file):
+    data = pandas.read_csv(data_file)
+    data = data[data["count"] > 20]
+
+    x = data["value"]
+    y = data["mean"]
+    error = data["std"]
+    upper = [y_val + e for y_val, e in zip(y, error)]
+    lower = [y_val - e for y_val, e in zip(y, error)]
+
+    p = figure(y_range=[1, 4])
+    p.scatter(x, y)
+
+    source = ColumnDataSource(data=dict(x=x, y=y, upper=upper, lower=lower))
+
+    p.add_layout(
+        Whisker(source=source, base="x", upper="upper", lower="lower", level="overlay")
+    )
+
+    return p
